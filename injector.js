79b2781facac9b777fcb8042f0af80308a993d03
Matheus Trindade <trindadematheus27@gmail.com>
2022-02-01 20:24:00 -0300

    toggle

--- injector.js
+++ injector.js
@@ -1,7 +1,39 @@
-const link = document.createElement("link");
+function toggleDodgerStyle() {
+  const dodgeStyle = document.getElementById("dodger-style");
 
-link.href = chrome.runtime.getURL("dodger.css");
-link.type = "text/css";
-link.rel = "stylesheet";
+  if (dodgeStyle) {
+    dodgeStyle.remove();
+  } else {
+    injectDodgerStyle();
+  }
+}
 
-document.getElementsByTagName("head")[0].appendChild(link);
+function injectDodgerButton() {
+  const link = document.createElement("link");
+  const buttonEl = document.createElement('button');
+
+  link.href = chrome.runtime.getURL("dodger-button.css");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+
+  buttonEl.className = 'dodger-button';
+  buttonEl.addEventListener('click', toggleDodgerStyle);
+  buttonEl.innerText = 'TOOGLE';
+
+  document.getElementsByTagName("head")[0].appendChild(link);
+  document.querySelector('#app').appendChild(buttonEl);
+}
+
+function injectDodgerStyle() {
+  const link = document.createElement("link");
+
+  link.href = chrome.runtime.getURL("dodger.css");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.id = "dodger-style"
+
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+injectDodgerStyle();
+injectDodgerButton();
